a8e840c294dcd0342ba947182f9da138581dc2ad
tonyalcorn <xmastony@gmail.com>
2020-08-30 21:38:39 -0700

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def add_post(post)
+        post.author = self
+      end
+      
+      def posts
+        Post.all.select {|post| post.author == self}
+      end
+      
+      def add_post_by_title(name)
+        post = Post.new(name)
+        post.author = self
+      end
+      
+      def self.post_count
+        Post.all.count
+      end
+    
+end
